74378714f9e84e08043864f77f6dba1bd349c3b2
Thea <theasamk@gmail.com>
2026-08-27 00:59:45 -0400

    array.js

--- week-2/array.js
+++ week-2/array.js
@@ -1,42 +1,105 @@
-//Accedding array elements
-// let fruits = ["Apple", "Banana", "Cherry"];
-// console.log(fruits[0]); //Apple
-// console.log(fruits[1]); //Banana
-// console.log(fruits[2]); //Cherry
+//Accessing Array Elements
 
-// //Updating element in an array
-// fruits[1] = "Mango"; //Apple, Mango, Cherry
-// console.log(fruits);
+let fruits = ["Apple", "Banana", "Charry"];
 
-// //Lenght
-// console.log(fruits.length);
+console.log(fruits[0]); //first element "Apple"
+console.log(fruits[1]); //second element "Banana"
+console.log(fruits[2]); //thirt element "Cherry"
 
-//concat
+//Updating Element in an Array
 
-// const colors = ["red", "blue"];
-// const moreColors = ["yellow", "white"];
-// const allColore = colors.concat(moreColors);
+fruits[1] = "Mango";
+console.log(fruits); //[ 'Apple', 'Mango', 'Charry' ]
 
-//flat
+//Array Properties .length
 
-//slice
-const grapes = ["a", "b", "c", "d", "e"];
-const slicedGrapes = grapes.slice(1, 4);
-console.log(grapes);
-console.log(slicedGrapes);
+console.log(fruits.length); //3 elements
 
-//Finders
-const cars = ["BWM", "KIA", "TESLA", "JEEP"];
-console.log(cars.indexOf("TESLA"));
+///Array Methods
+
+//concat ()
+const colors = ["red", "blue"];
+const moreColors = ["yellow", "white"];
+const allcolors = colors.concat(moreColors);
+
+console.log(colors); //[ 'red', 'blue' ]
+console.log(moreColors); //[ 'yellow', 'white' ]
+console.log(allcolors); //[ 'red', 'blue', 'yellow', 'white' ]
+
+//flat ()
+const nestedArr = [
+  [1, 2],
+  [3, 4],
+  [5, 6, 7, 8],
+];
+const flattenedArr = nestedArr.flat();
+console.log(nestedArr[2].length); // 4
+
+console.log(nestedArr);
+console.log(flattenedArr);
+
+//slice()
+const grapes = ["A", "B", "C", "D", "E"];
+const slicedGrapes = grapes.slice(1, 4); // ["A", "B", "C", "D", "E"];
+console.log(slicedGrapes); //[ 'B', 'C', 'D' ]
+
+///The Mutators
+
+const mutableFruits = [...fruits]; //[ 'Apple', 'Mango', 'Charry']
+console.log(fruits); //[ 'Apple', 'Mango', 'Charry']
+
+//push -add an element to the end
+mutableFruits.push("kiwi");
+console.log(mutableFruits);
+
+//pop - removes the last element
+mutableFruits.pop();
+console.log(mutableFruits);
+
+//unshift - adds an element to the start
+mutableFruits.unshift("grapes");
+console.log(mutableFruits);
+
+//shift -removes the first element
+mutableFruits.shift();
+console.log(mutableFruits); //[ 'Apple', 'Mango', 'Charry' ]
+
+//reverse() - reverse the order
+mutableFruits.reverse();
+console.log(mutableFruits);
+
+//sort
+mutableFruits.sort();
+console.log(mutableFruits); //[ 'Apple', 'Charry', 'Mango' ]
+
+//check the original array
+console.log(fruits);
+
+///The Finders
+//Indexof
+const cars = ["BMD", "Mercedes", "Tesla", "KIA", "Jeep"];
+console.log(cars.indexOf("Tesla")); //2
+
+//lastIndexof()
+console.log(cars.lastIndexOf("KIA"));
+
+//at
+console.log(cars.at(2));
 
 //includes
+console.log(cars.includes("Jeep"));
 
-console.log(cars.includes("KIA"));
+//The Joiners & Converters
+//join()
+console.log(cars.join("/ "));
 
-///joiners & convertors
-
-console.log(cars.join(" / "));
-
-//from
+//Array.from()
 const str = "Hello";
-const chars = Array.from(str);
+console.log(Array.from(str)); //[ 'H', 'e', 'l', 'l', 'o' ]
+
+//Array.isArray()
+const luckyNumbers = [7, 10, 19];
+console.log(Array.isArray(luckyNumbers)); //true
+
+const studentName = "Alice";
+console.log(Array.isArray(studentName)); //false
