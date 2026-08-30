99d5610eecf8805f3a517f0291b5d566f04da973
Thea <theasamk@gmail.com>
2026-08-30 17:29:57 -0400

    massage

---
+++ Homework/homework4String.js
@@ -0,0 +1,55 @@
+// let duble = "NYC";
+// console.log(duble);
+
+// let backtick = `NYC`;
+// console.log(backtick);
+
+// let city = "Chicago";
+// console.log(city.length);
+
+// let cityOne = "Chicago";
+// console.log(cityOne[0]);
+// console.log(cityOne[6]);
+
+// let cityOne = "Chicago";
+// console.log(cityOne.toUpperCase());
+// console.log(cityOne.toLowerCase());
+
+// let state = "   Georgia       ";
+// console.log(state.trim());
+
+// let state = "NewYork";
+// console.log(state.slice(0, 3));
+// console.log(state.slice(3));
+
+// let location = "I live in Chicago";
+// console.log(location.replace("Chicago", "New York"));
+
+// let drink = "i love coffee";
+// console.log(drink.includes("love"));
+// console.log(drink.includes("hate"));
+
+let drinks = "coffee, water, tea, juice ";
+console.log(drinks.split(","));
+console.log(drinks.split(""));
+
+let firstName = "Russell";
+lastName = "Crowe";
+console.log(firstName.concat(" ", lastName));
+
+let amazon = "PrimeVideo";
+console.log(amazon.startsWith("Prime"));
+console.log(amazon.endsWith("Video"));
+
+let company = "facebook";
+company = "WhatsApp";
+let greeting = `Social Media Network, ${company}`;
+console.log(greeting);
+
+let a = 25;
+b = 50;
+console.log(`The sum of ${a} and ${b} is ${a + b}`);
+
+let phone = "Iphone Pro Max 17";
+price = 1200;
+console.log(`apple: ${phone.toUpperCase()}, price: ${price}`);
