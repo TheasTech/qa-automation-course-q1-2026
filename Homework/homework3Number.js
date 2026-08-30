3c907a63d2420f60cd16f6296f1dffc283ba7a21
Thea <theasamk@gmail.com>
2026-08-30 15:23:07 -0400

    massage

---
+++ Homework/homework3Number.js
@@ -0,0 +1,65 @@
+// ///Number type
+// let age = 20;
+// console.log(age);
+// let price = 2.55;
+// console.log(price);
+// let temperature = -5;
+// console.log(temperature);
+
+// ///Number are different from string:
+
+// let x = 1; //number
+// let y = "1"; //string
+// console.log(x);
+// console.log(y);
+// console.log(typeof x); //console.log(typeOf 5);
+// console.log(typeof y); //console.log(typeOf "5")
+
+// console.log(20 + 10);
+// console.log(20 - 10);
+// console.log(20 * 10);
+// console.log(21 / 10);
+// console.log(21 % 10); //remainder
+
+// ///Corporisons true or false
+// console.log(20 > 10);
+// console.log(20 < 10);
+// console.log(20 === 20);
+// console.log(20 !== 10);
+
+// ///Rounding numbers
+// console.log(Math.round(2.2));
+// console.log(Math.round(3.99));
+
+// console.log(Math.floor(2.2));
+// console.log(Math.floor(3.99));
+
+// console.log(Math.ceil(7.7));
+// console.log(Math.ceil(7.2));
+
+// console.log(Math.trunc(2.5));
+// console.log(Math.trunc(-1.2));
+
+// let price = 5.6789;
+// console.log(price.toFixed(2));
+
+// console.log(Number("42"));
+// console.log(Number("name"));
+
+console.log(parseInt("5.5"));
+console.log(parseFloat("5.5"));
+
+console.log(isNaN("name"));
+console.log(isNaN(10));
+
+console.log(Math.abs(-5));
+
+console.log(Math.min(5, 4, 3, 2));
+console.log(Math.max(5, 4, 3, 2));
+
+console.log(Math.pow(3, 3));
+
+let rendom10to20 = Math.floor(Math.random() * (20 - 10 + 1)) + 1;
+console.log(rendom10to20);
+
+console.log(Math.sqrt(25));
