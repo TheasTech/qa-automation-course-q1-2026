70d6192b52de88ddbc414a9eecd07fd5634c58e9
Thea <theasamk@gmail.com>
2026-08-30 23:38:07 -0400

    massage

--- Homework/arrays-strings-numbers.js
+++ Homework/arrays-strings-numbers.js
@@ -48,11 +48,9 @@ console.log("join():", carBrandsString); // Convert the car brands array into a 
 console.log("Array.isArray(carBrands):", Array.isArray(carBrands)); // Check if the car brands variable is an array
 // Convert the word `"ENGINE"` into an array of characters
 
-//An array of **numbers**
-let number = [10, 20, 30, 40, 50];
+let number = [10, 20, 30, 40, 50]; //An array of **numbers**
 
-//A **string** with extra spaces
-let string = "Macbook Air ";
+let string = "Macbook Air "; //A **string** with extra spaces
 
 console.log(string.trim()); // Remove extra spaces from the string
 // Log the cleaned string
@@ -81,4 +79,25 @@ console.log(strOne.concat(" ", strTwo)); // Join two strings using a method (not
 const carBrand = "Tesla"; //car brand
 const carYear = 2024; //car year (has to be in the past)
 console.log(2026 - carYear); //car age (think about how you get the car age)
-//A **decimal number**
+
+let decimalNumber = 5.9; //A **decimal number**
+console.log(Math.round(decimalNumber)); // Round a decimal normally
+console.log(Math.floor(decimalNumber)); // Always round down
+console.log(Math.ceil(decimalNumber)); // Always round up
+console.log(Math.trunc(decimalNumber)); // Remove the decimal part
+
+console.log((5.987765).toFixed(2)); //Format a number to **2 decimal places**
+// Explain (in comments) why the result is **not a number**
+//console.log("5"); //Convert a numeric string into a number
+//console.log("5.5"); //Convert a decimal string into an integer
+// Convert a decimal string into a float
+console.log(isNaN("BMD")); //Check if a value is **Not a Number**
+// Check if a number is an integer
+//Find the absolute value of a number
+console.log(Math.min(1, 4, 7)); // Find the smallest number
+console.log(Math.max(1, 4, 7)); //Find the largest number
+console.log(Math.pow(4, 5)); //Raise a number to a power
+//Find the square root of a number
+// Generate a random number between 0 and 1
+// Generate a random whole number between **1 and 10**
+// Explain the formula in comments
