63dd18688acbee658c84fd8b03a7120b26f1fffc
Thea <theasamk@gmail.com>
2026-09-06 21:07:28 -0400

    Functions

--- week-3/function.js
+++ week-3/function.js
@@ -1,45 +1,91 @@
-///Function Syntax
+//1.Creating a functions
 function greet() {
   console.log("Hello, World");
 }
+greet(); //greet() - calling the function.
 
-greet();
+//2. Function with a single parametr
+function newGreet(name) {
+  console.log(`Hello ${name} welcome to the class!`);
+}
+newGreet("smith");
 
-///Parametres
-function greet(name) {
-  console.log("Hello, " + name + "!");
+function guest(number) {
+  console.log(`Hello Guest! ${number} your order is ready!`);
+}
+guest(15);
+guest(10);
+
+//3. Function with multiple parametrs
+
+function add(num1, num2, num3, num4, num5) {
+  console.log(num1);
+  console.log(num2);
+  console.log(num3);
+  console.log(num4);
+  console.log(num5);
 }
 
-greet("Alice");
-greet("Bob");
-greet("NYC");
+add(1, 2, 3, 4, 5);
 
-///Multiple Parametres
-function add(a, b) {
-  console.log(a + b);
+function multiplication(num1, num2) {
+  console.log(num1 * num2);
 }
-add(5, 3);
+multiplication(4, 2);
 
-// ///Default parameters
-function greet(name = "guest") {
-  console.log("Hello, " + name + "!");
+function printNumber(num1, numb2) {
+  if (num1 > numb2) {
+    console.log("Number is bigger");
+  } else {
+    console.log("Number is smaller");
+  }
+}
+printNumber(1, 2);
+printNumber(5, 4);
+
+// 4. Default Parametrs
+
+function greetStudent(name = "student") {
+  console.log(`Hello ${name}, welcome to JavaScreipt class!`);
+}
+greetStudent();
+greetStudent("Alex");
+
+// 5. Return values
+function addNewNumbers(num1, num2) {
+  return num1 + num2;
 }
 
-greet();
-greet("Aliece");
+const functionrResult = addNewNumbers(3, 6);
+console.log(functionrResult);
 
-///Return Value
-function add(a, b) {
-  return a + b;
+//or
+function addNewNumbersTwo(num1, num2) {
+  const result = num1 + num2;
+  return result;
 }
-let result = add(5, 3);
-console.log(result);
 
-///Arrow Function
-const greet = (name) => {
-  console.log("Hello, " + name + "!");
+const functionrResultTWo = addNewNumbersTwo(3, 6);
+console.log(functionrResultTWo);
+
+// 5. Arrow Functions
+
+const greetStudentTwo = (student) => {
+  console.log(`Hello ${student}, welcome to class!`);
 };
-greet("Charlie");
+greetStudentTwo("Davit");
 
-const greetShort = (name) => console.log("Hello, " + name + "!");
-greetShort("Dave");
+//another
+
+const substractTwoNumbers = (num1, num2) => num1 - num2;
+const resultTwo = substractTwoNumbers(5, 3);
+
+console.log(resultTwo);
+
+function generateRandomNumber() {
+  return 2;
+}
+const array = ["Red", "Blue", "Green"];
+const rendomNumber = generateRandomNumber();
+
+console.log(array[rendomNumber]);
